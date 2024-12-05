3f857aa3f0b2efdb5949dc1def9593a176adb2c6
Macsim Minakov <maksim.minakov.83@mail.ru>
2024-12-05 14:01:13 +0300

    1.1 - Get-запрос - Задача

--- src/features/decks/DecksList/DecksList.tsx
+++ src/features/decks/DecksList/DecksList.tsx
@@ -1,5 +1,16 @@
 import s from './DecksList.module.css'
+import { useEffect } from 'react'
+import { decksApi } from '../decks-api.ts'
 
 export const DecksList = () => {
+
+  useEffect(() => {
+    decksApi.fetchDecks()
+  }, [])
+
+
+
+
+
   return <ul className={s.list}></ul>
 }
--- src/features/decks/decks-api.ts
+++ src/features/decks/decks-api.ts
@@ -6,3 +6,43 @@ export const instance = axios.create({
     'x-auth-skip': true,
   },
 })
+
+
+export const decksApi = {
+  fetchDecks(){
+    return instance.get<Decks>('/v2/decks')
+  }
+}
+
+
+
+
+interface Author {
+  id: string;
+  name: string;
+}
+
+interface Item {
+  isFavorite: boolean;
+  author: Author;
+  id: string;
+  userId: string;
+  name: string;
+  isPrivate: boolean;
+  cover: string;
+  created: string; // ISO 8601 date string
+  updated: string; // ISO 8601 date string
+  cardsCount: number;
+}
+
+interface Pagination {
+  currentPage: number;
+  itemsPerPage: number;
+  totalPages: number;
+  totalItems: number;
+}
+
+interface Decks {
+  items: Item[];
+  pagination: Pagination;
+}
